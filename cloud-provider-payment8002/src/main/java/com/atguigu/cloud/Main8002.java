a36d7b793260da052ebdc5fd095d268f1b2d0f71
Aijinhui <ajhmcs@163.com>
2024-05-26 16:51:01 +0800

    loadbalancer

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/cloud/Main8002.java
@@ -0,0 +1,21 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @ Author：Aijinhui
+ * @ Description：
+ * @ Date：2024-05-24-17:12
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
+@MapperScan("com.atguigu.cloud.mapper")  // 扫描mapper接口
+@RefreshScope  // 使得consul的服务器配置能够实时刷新，不配置的话默认是55s刷新一次，在bootstrap.yml下也能手动调整，用watch
+public class Main8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8002.class, args);
+    }}
---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -0,0 +1,82 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.responce.ResultData;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @ Author：Aijinhui
+ * @ Description：
+ * @ Date：2024-05-24-18:15
+ */
+
+@RestController
+@Tag(name = "支付微服务模块",description = "支付CRUD")  // swagger注解
+public class PayController
+{
+    @Resource
+    PayService payService;
+
+    @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")  // swagger注解
+    public ResultData<String> addPay(@RequestBody Pay pay)
+    {
+        System.out.println(pay.toString());
+        int i = payService.add(pay);
+        return ResultData.success("成功插入记录，返回值："+i);
+    }
+
+    @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "删除",description = "删除支付流水方法")
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
+    }
+
+    @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改",description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO)
+    {
+        Pay pay = new Pay();
+        BeanUtils.copyProperties(payDTO, pay);
+
+        int i = payService.update(pay);
+        return ResultData.success("成功修改记录，返回值："+i);
+    }
+
+    @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/all")
+    @Operation(summary = "查询全部流水",description = "查询全部流水方法")
+    public ResultData<List<Pay>> getAll()
+    {
+        List<Pay> all = payService.getAll();
+        return ResultData.success(all);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping(value = "/pay/get/info")
+    private String getInfoByConsul(@Value("${atguigu.info}") String atguiguInfo)
+    {
+        return "atguiguInfo: "+atguiguInfo+"\t"+"port: "+port;
+    }
+
+}
+
